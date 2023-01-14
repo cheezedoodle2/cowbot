68cdfe6f28ca325ab895269ddaafc7cc3b04dea5
cheezedoodle2 <cheezedoodle2@users.noreply.github.com>
2023-01-13 22:42:47 -0800

    🐮

--- events/MessageCreate.js
+++ events/MessageCreate.js
@@ -5,9 +5,8 @@ module.exports = {
 	async execute(message) {
 		if (message.author.bot) return;
         //console.log("message: " + message.cleanContent);
-        if (message.author == "551571201437466625" &&
-            message.cleanContent.search("e-girl") >= 0) {
-                message.react("I'm not an e-girl!  -- Brooke");
+        if (message.cleanContent.search("e-girl") >= 0) {
+            message.reply("I'm not an e-girl!  -- Brooke");
         }
         if (message.cleanContent.search("moo") >= 0) {
             try {
